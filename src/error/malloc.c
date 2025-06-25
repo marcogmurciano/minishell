83421de7ad6b2683042efd03db2fa4f54d545d86
marcogmurciano <marcogmurciano@gmail.com>
2025-06-26 00:00:00 +0200

    feat: Implement error handling and validation

---
+++ src/error/malloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 14:52:00 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:38:01 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Print malloc error, clear history and exit
+ */
+void	malloc_error(t_minishell *minishell)
+{
+	free_minishell(minishell);
+	minishell = NULL;
+	write(STDERR_FILENO, "malloc error", 12);
+	rl_clear_history();
+	exit(EXIT_SUCCESS);
+}
