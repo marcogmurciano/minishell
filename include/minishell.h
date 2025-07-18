15dab5f2a0d3c9043bef15e2fd7665e8f8d52700
d3bvstack <dbarba-v@student.42madrid.com>
2025-07-18 16:58:58 +0200

    Refactor .h and small fixes

--- include/minishell.h
+++ include/minishell.h
@@ -18,119 +18,12 @@
 #include "readline/readline.h"
 #include <signal.h>
 #include "pipex.h"
+#include "structs.h"
 
 /**
  * Signals global variable
  */
 extern volatile sig_atomic_t g_signal_status;
-
-/**
- * Types of tokens
- */
-// typedef enum		e_token_type
-// {
-// 	TOKEN_EOF,			  // 0
-// 	TOKEN_WORD,			  // 1
-// 	TOKEN_PIPE,			  // 2
-// 	TOKEN_REDIR_IN,		  // 3
-// 	TOKEN_REDIR_OUT,	  // 4
-// 	TOKEN_HEREDOC,		  // 5
-// 	TOKEN_APPEND,		  // 6
-// 	//
-// 	// 	v EXTRAS ASSIGNED DURING REFINING PHASE v
-// 	//
-// 	TOKEN_REDIR_IN_FILE,  // 7
-// 	TOKEN_REDIR_OUT_FILE, // 8
-// 	TOKEN_HEREDOC_DELIM,  // 9
-// 	TOKEN_APPEND_FILE,    // 10
-// 	TOKEN_CMD,			  // 11
-// 	TOKEN_ARG			  // 12
-// } 					t_token_type;
-
-// /**
-//  * Types of quotes
-//  */
-// typedef enum		e_quote_type
-// {
-// 	NON_QUOTE,
-// 	SINGLE_QUOTE,
-// 	DOUBLE_QUOTE
-// } 					t_quote_type;
-
-// /**
-//  * Command structure
-//  */
-// typedef struct		s_cmd t_cmd;
-// typedef struct		s_cmd
-// {
-// 	char			**argv;		// Array of arguments, [0] is command itshelf
-// 	char			*infile;	// If REDIR_IN present
-// 	char			*outfile;	// If REDIR_OUT present
-// 	int				append;     // If APPEND present
-// 	char			*heredoc;	// If HEREDOC present // This is the path to temp file
-// 	t_cmd			*next;
-// } 					t_cmd;
-
-/**
- * Expansion auxiliary structure
- */
-typedef struct		s_expansion
-{
-	char			*word_value;
-	int				dollar_position;
-	int				variable_start;
-	int				variable_name_length;
-	char			*variable_name;
-	char			*variable_value;
-	char			*preffix;
-	char			*suffix;
-	char			*new_word_value;
-} 					t_expansion;
-
-/**
- * Segment structure
- */
-typedef struct		s_segment
-{
-	char			**token_value;
-	int				next_segment_start;
-} 					t_segment;
-
-// /**
-//  * Minishell structure
-//  * Dependencies:
-//  * - Environment structure
-//  * - Token structure
-//  */
-// typedef struct		s_env t_env;
-// typedef struct		s_env
-// {
-// 	char			*key;
-// 	char			*value;
-// 	t_env			*next;
-// } 					t_env;
-
-// typedef struct		s_token
-// {
-// 	char			*value;
-// 	t_quote_type	quote_type;
-// 	t_token_type	token_type;
-// 	struct s_token	*next;
-// 	struct s_token	*prev;
-// } 					t_token;
-
-// typedef struct		s_minishell
-// {
-// 	char			**envp;
-// 	char			*input;
-// 	int				last_exit_status;		// "$?"
-// 	int				duplicated_std_fds[2];
-// 	int				pid;
-// 	t_token			*tokens_list;
-// 	t_env			*environment;
-// 	t_cmd			*cmd_pipelines;
-// } 					t_minishell;
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //
--- include/pipex.h
+++ include/pipex.h
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+#define PIPEX_H
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,82 +20,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
-
-
-
-///////////////////////////////////////
-
-typedef enum		e_token_type
-{
-	TOKEN_EOF,			  // 0
-	TOKEN_WORD,			  // 1
-	TOKEN_PIPE,			  // 2
-	TOKEN_REDIR_IN,		  // 3
-	TOKEN_REDIR_OUT,	  // 4
-	TOKEN_HEREDOC,		  // 5
-	TOKEN_APPEND,		  // 6
-	//
-	// 	v EXTRAS ASSIGNED DURING REFINING PHASE v
-	//
-	TOKEN_REDIR_IN_FILE,  // 7
-	TOKEN_REDIR_OUT_FILE, // 8
-	TOKEN_HEREDOC_DELIM,  // 9
-	TOKEN_APPEND_FILE,    // 10
-	TOKEN_CMD,			  // 11
-	TOKEN_ARG			  // 12
-} 					t_token_type;
-
-/**
- * Types of quotes
- */
-typedef enum		e_quote_type
-{
-	NON_QUOTE,
-	SINGLE_QUOTE,
-	DOUBLE_QUOTE
-} 					t_quote_type;
-
-typedef struct		s_cmd t_cmd;
-typedef struct		s_cmd
-{
-	char			**argv;		// Array of arguments, [0] is command itshelf
-	char			*infile;	// If REDIR_IN present
-	char			*outfile;	// If REDIR_OUT present
-	int				append;     // If APPEND present
-	char			*heredoc;	// If HEREDOC present // This is the path to temp file
-	t_cmd			*next;
-} 					t_cmd;
-
-typedef struct		s_env t_env;
-typedef struct		s_env
-{
-	char			*key;
-	char			*value;
-	t_env			*next;
-} 					t_env;
-
-typedef struct		s_token
-{
-	char			*value;
-	t_quote_type	quote_type;
-	t_token_type	token_type;
-	int             spaced;
-	struct s_token	*next;
-	struct s_token	*prev;
-} 					t_token;
-
-typedef struct		s_minishell
-{
-	char			**envp;
-	char			*input;
-	int				last_exit_status;		// "$?"
-	int				duplicated_std_fds[2];
-	int				pid;
-	t_token			*tokens_list;
-	t_env			*environment;
-	t_cmd			*cmd_pipelines;
-} 					t_minishell;
-
+#include "structs.h"
 
 ///////////////////////////////////////////////
 typedef struct s_fds
@@ -110,7 +38,6 @@ typedef struct s_fds
 	int			has_outfile;
 	t_minishell	*minishell;
 }			t_fds;
-
 
 int		process_cmds_errors(char *av[], int i, char **envp);
 void	build_is_pathed(int ac, char *av[], char **is_pathed, int has_infile);
@@ -133,3 +60,5 @@ void	manage_parent_fds(t_fds *fd, int *pipes, int i);
 void	print_child_error(t_fds *fd);
 void	only_child(t_fds *fd, char *cmd);
 int		ft_pipex(int ac, char *av[], t_minishell *minishell, int has_files);
+
+#endif
