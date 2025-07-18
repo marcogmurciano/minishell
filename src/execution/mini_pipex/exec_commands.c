203cb31cf01814ca0b92b16fd041e7d94f8e893b
d3bvstack <dbarba-v@student.42madrid.com>
2025-07-18 18:35:17 +0200

    changed comparison to ft_ from libft

--- src/execution/mini_pipex/exec_commands.c
+++ src/execution/mini_pipex/exec_commands.c
@@ -27,19 +27,19 @@ static void	manual_execution(char *cmd, t_fds *fd)
 	split_cmd = ft_split(cmd, ' ');
 	if (!split_cmd)
 		exit(1);
-	// if (strcmp(split_cmd[0], "echo") == 0)
+	// if (ft_strcmp(split_cmd[0], "echo") == 0)
     // 	// builtin_echo();
-	if (strcmp(split_cmd[0], "export") == 0)
+	if (ft_strcmp(split_cmd[0], "export") == 0)
 	    builtin_export(fd->minishell, split_cmd);
-	// if (strcmp(split_cmd[0], "cd") == 0)
+	// if (ft_strcmp(split_cmd[0], "cd") == 0)
 	//     // builtin_cd();
-	if (strcmp(split_cmd[0], "pwd") == 0)
+	if (ft_strcmp(split_cmd[0], "pwd") == 0)
 	    builtin_pwd();
-	// if (strcmp(split_cmd[0], "unset") == 0)
+	// if (ft_strcmp(split_cmd[0], "unset") == 0)
 	//     // builtin_unset();
-	if (strcmp(split_cmd[0], "env") == 0)
+	if (ft_strcmp(split_cmd[0], "env") == 0)
 	    builtin_env(fd->minishell, split_cmd);
-	// if (strcmp(split_cmd[0], "exit") == 0)
+	// if (ft_strcmp(split_cmd[0], "exit") == 0)
 	//     // builtin_exit();
 
 	// cleanup
