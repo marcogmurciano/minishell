4239f4b1f31803fce57f74bdf02e0b9ce4243e55
d3bvstack <dbarba-v@student.42madrid.com>
2025-07-18 18:36:20 +0200

    Updated export, needs execution test

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export.c                                           :+:      :+:    :+:   */
+/*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 11:28:46 by dbarba-v          #+#    #+#             */
-/*   Updated: 2025/07/16 17:33:39 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/07/18 18:20:58 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,45 @@ static int check_key_syntax(char *key)
 	return (0);
 }
 
+void ft_swap(char **first, char **second)
+{
+	char *temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
+}
+
+void print_ordered_envp(t_minishell *minishell)
+{
+	int		env_count;
+	int		i;
+	int		j;
+	char	**sorted_envp;
+
+	env_count = 0;
+	while(minishell->envp[env_count])
+		env_count++;
+	sorted_envp = ft_calloc(env_count, sizeof(char *));
+	i = 0;
+	while (i++ < env_count)
+		sorted_envp[i] = ft_strdup(minishell->envp[i]);
+	i = 0;
+	while (i++ < env_count - 1)
+	{
+		j = 0;
+		while(j++ < env_count - 1)
+		{
+			if (ft_strcmp(sorted_envp[i], sorted_envp[i + 1]) > 0)
+				ft_swap(&sorted_envp[i], &sorted_envp[i + 1]);
+		}
+	}
+	i = 0;
+	while (sorted_envp[i++])
+		printf("declare -x %s\n", sorted_envp[i]);
+	ft_free_array((void **)sorted_envp);
+}
+
 /**
  * @brief Exports environment variables to the minishell environment.
  *        Adds new or replaces existing environment variables as needed.
@@ -91,5 +130,7 @@ int	builtin_export(t_minishell *minishell, char **argv)
 		minishell->envp = get_environment_array(minishell->environment);
 		i++;
 	}
+	if (i == 1)
+		print_ordered_envp(minishell);
 	return (0);
 }
