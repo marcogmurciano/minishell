53b3251cd4961f3b70db64ad2583f68b7107708c
d3bvstack <dbarba-v@student.42madrid.com>
2025-07-22 16:52:05 +0200

    echo builtin fix

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 11:54:42 by marcoga2          #+#    #+#             */
-/*   Updated: 2025/07/22 12:27:17 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/07/22 16:28:09 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 int builtin_echo(t_minishell *minishell, char **argv)
 {
-    int i;
+    int i = 1;
+    int newline = 1;
 
     (void)minishell;
-    i = 1;
-    while (argv[i])
+    if (argv[i] && strcmp(argv[i], "-n") == 0)
     {
-        if(strcmp(argv[1], "-n"))
-        {
-            ft_putstr_fd(argv[i], STDIN_FILENO);
-        }
-        else
-        {
-            ft_putendl_fd(argv[i], STDIN_FILENO);
-        }
+        newline = 0;
         i++;
     }
-    return (0);
+    while (argv[i])
+    {
+        ft_putstr_fd(argv[i], STDOUT_FILENO);
+        if (argv[i + 1])
+            ft_putstr_fd(" ", STDOUT_FILENO);
+        i++;
+    }
+    if (newline)
+        ft_putstr_fd("\n", STDOUT_FILENO);
+    return 0;
 }
