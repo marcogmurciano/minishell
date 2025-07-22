1db9d66e81c8312d348c8445d04a3450b6ff37f9
marcogmurciano <marcogmurciano@gmail.com>
2025-07-22 12:46:34 +0200

    commit my changes in 'before_last_merge'

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -132,24 +132,12 @@ int execution(t_minishell *minishell)
     t_cmd *head = minishell->cmd_pipelines;
     int cmd_count;
     int result;
-    // char **full_pipe_line;
-    // int files_flag;
 
-    // files_flag = 11;
     cmd_count = ft_cmdsize(head);
     if (!head)
         return (1);
-    // //debug
-    // print_cmd(head);
-    //
-    // full_pipe_line = create_cmd_array(head, &cmd_count, &files_flag);
-    // if (!full_pipe_line)
-    //     return (1);
-    // //debug
-    // print_ft_pipex_arguments(cmd_count, full_pipe_line, files_flag);
-    //
+    printf("holaaa\n");
     result = ft_pipex(cmd_count, head, minishell);
-    //ft_free_array((void *)full_pipe_line);
     return (result);
 }
 
