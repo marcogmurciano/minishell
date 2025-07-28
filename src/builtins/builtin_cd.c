cff886ef934ab3c0badbb7c71632972bb4cff650
Diego <180553755+d3bvstack@users.noreply.github.com>
2025-07-28 10:31:16 +0000

    Fixed duplicated logic

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,35 +6,27 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 12:28:24 by dbarba-v          #+#    #+#             */
-/*   Updated: 2025/07/28 11:44:35 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/07/24 18:39:09 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 int    builtin_cd(t_minishell *minishell, char **argv)
-int    builtin_cd(t_minishell *minishell, char **argv)
 {
-	char *directory;
 	char *directory;
     int argc;
 	int chdir_status;
 	int i;
-	int i;
 
-	(void) minishell;
 	(void) minishell;
 	argc = 0;
 	chdir_status = 0;
 	i = 0;
-	i = 0;
 	while (argv[argc])
 		argc++;
 	directory = ft_strjoin(getenv("PWD"), getenv("HOME"));
-	directory = ft_strjoin(getenv("PWD"), getenv("HOME"));
 	if(argc == 1)
-		chdir_status = chdir(getenv("HOME"));
-	else
 		chdir_status = chdir(getenv("HOME"));
 	else
 	{
@@ -46,9 +38,6 @@ int    builtin_cd(t_minishell *minishell, char **argv)
 	}
 
 	if(chdir_status == -1)
-		perror("minishell: cd1");
-	free(directory);
-    return ((unsigned int)chdir_status);
 		perror("minishell: cd1");
 	free(directory);
     return ((unsigned int)chdir_status);
