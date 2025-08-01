678c752d7c714a732bb8a3725cc7e7198a7faba1
d3bvstack <dbarba-v@student.42madrid.com>
2025-08-01 11:56:53 +0200

    Fix builtin exit

--- src/execution/mini_pipex/exec_commands.c
+++ src/execution/mini_pipex/exec_commands.c
@@ -28,9 +28,9 @@ int execute_built_in(t_minishell *minishell, char **split_cmd)
 	else if (ft_strcmp(split_cmd[0], "env") == 0)
 		status = builtin_env(minishell, split_cmd);
 	else if (ft_strcmp(split_cmd[0], "cd") == 0)
-	    builtin_cd(minishell, split_cmd);
+	    status = builtin_cd(minishell, split_cmd);
 	else if (ft_strcmp(split_cmd[0], "exit") == 0)
-	    builtin_exit(minishell, split_cmd);
+	    status = builtin_exit(minishell, split_cmd);
 	
 	return(status);
 }
