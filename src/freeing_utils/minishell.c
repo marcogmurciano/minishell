aa51dc4572b5998003f97f78bc67623d2c074323
d3bvstack <180553755+d3bvstack@users.noreply.github.com>
2025-08-08 22:04:03 +0100

    Fix file descriptor close

--- src/freeing_utils/minishell.c
+++ src/freeing_utils/minishell.c
@@ -22,7 +22,7 @@ static void	close_dup_stds(t_minishell *minishell)
 		close(minishell->duplicated_std_fds[0]);
 		minishell->duplicated_std_fds[0] = -1;
 	}
-	if(minishell->duplicated_std_fds[0] != -1)
+	if(minishell->duplicated_std_fds[1] != -1)
 	{
 		close(minishell->duplicated_std_fds[1]);
 		minishell->duplicated_std_fds[1] = -1;
