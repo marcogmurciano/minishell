10f7fb08a8b5b790d95dc4f914f951eaf04747e9
d3bvstack <dbarba-v@student.42madrid.com>
2025-08-12 14:51:05 +0200

    Fixed unset of first node

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -13,34 +13,59 @@
 #include "../../include/minishell.h"
 
 /**
+ * Unsets a node on other than the first position of the environ linked list
+ */
+static void unset_nonfirst(t_minishell *minishell, t_env *current)
+{
+	t_env	*temp_env;
+	
+	temp_env = current->next;
+	current->next = temp_env->next;
+	temp_env->next = NULL;
+	free_environment(&temp_env);
+	ft_free_array((void **)(minishell->envp));
+	minishell->envp = get_environment_array(minishell->environment);
+}
+
+/**
+ * Unsets node at first position of the linked list
+ */
+static void unset_first(t_minishell *minishell, t_env *current)
+{
+	minishell->environment = current->next;
+	current->next = NULL;
+	free_environment(&current);
+	ft_free_array((void **)(minishell->envp));
+	minishell->envp = get_environment_array(minishell->environment);
+}
+
+/**
  * If environment variable exists, remove from environment
  */
 int	builtin_unset(t_minishell *minishell, char **argv)
 {
 	t_env	*current_env;
-	t_env	*temp_env;
 	int		i;
 
-	i = 1;
-	while (argv[i])
+	i = 0;
+	while (argv[++i])
 	{
 		current_env = minishell->environment;
+		if (current_env && ft_strcmp(current_env->key, argv[i]) == 0)
+		{
+			unset_first(minishell, current_env);
+			continue;
+		}
 		while (current_env)
 		{
 			if (current_env->next && ft_strcmp(current_env->next->key,
 					argv[i]) == 0)
 			{
-				temp_env = current_env->next;
-				current_env->next = temp_env->next;
-				temp_env->next = NULL;
-				free_environment(&temp_env);
-				ft_free_array((void **)(minishell->envp));
-				minishell->envp = get_environment_array(minishell->environment);
-				continue ;
+				unset_nonfirst(minishell, current_env);
+				break ;
 			}
 			current_env = current_env->next;
 		}
-		i++;
 	}
 	return (0);
 }
