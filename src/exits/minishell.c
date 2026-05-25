407091e17c2d40bd42b3d418abe985175079c870
marcogmurciano <marcogmurciano@gmail.com>
2026-05-25 13:23:18 +0200

    feat: Implement main entry point and exit handlers

---
+++ src/exits/minishell.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 17:53:36 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:37:44 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Cleans up resources and exits the minishell program.
+ */
+void	exit_minishell(t_minishell *minishell)
+{
+	int	exit_status;
+
+	exit_status = minishell->last_exit_status;
+	free_minishell(minishell);
+	minishell = NULL;
+	write(STDOUT_FILENO, "exit\n", 5);
+	rl_clear_history();
+	exit(exit_status);
+}
---
+++ src/main.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 16:06:14 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/08/09 16:13:01 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+volatile sig_atomic_t	g_signal_status = 0;
+
+void	get_signal_exit(t_minishell *minishell)
+{
+	(void)minishell;
+	if (g_signal_status == SIGINT)
+	{
+		minishell->last_exit_status = 130;
+		g_signal_status = 0;
+	}
+	else if (g_signal_status == SIGQUIT)
+	{
+		g_signal_status = 0;
+	}
+}
+
+static void	minishell_loop(t_minishell *minishell)
+{
+	while (1)
+	{
+		setup_signal_handlers();
+		minishell->input = get_prompt_input(minishell);
+		get_signal_exit(minishell);
+		if (minishell->input == NULL)
+			exit_minishell(minishell);
+		if (tokenization(minishell) == 1)
+			continue ;
+		if (syntax_analysis(minishell) == 1)
+		{
+			free_tokens_list(&(minishell->tokens_list));
+			continue ;
+		}
+		minishell->last_exit_status = execution(minishell);
+		free_cmds(&(minishell->cmd_pipelines));
+		free_tokens_list(&(minishell->tokens_list));
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_minishell	minishell;
+
+	init_mini(&minishell, argc, argv, envp);
+	minishell_loop(&minishell);
+	return (0);
+}
