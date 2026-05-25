3dab8b439f25b0bdf892bd433d504fd4e62fdf2d
marcogmurciano <marcogmurciano@gmail.com>
2026-05-25 13:23:12 +0200

    feat: Implement error handling and validation

---
+++ src/error/syntax.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   syntax.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 12:03:58 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:37:51 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Printing of syntax errors
+ */
+int	syntax_error(char *origin, t_minishell *minishell, int code)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	if (origin)
+	{
+		ft_putstr_fd(origin, STDERR_FILENO);
+		ft_putstr_fd(":  ", STDERR_FILENO);
+	}
+	if (code == 0)
+		ft_putstr_fd("\033[0;31msyntax error\033[0m: unsupported character",
+			STDERR_FILENO);
+	else if (code == 1)
+		ft_putstr_fd("\033[0;31msyntax error\033[0m: invalid file redirection",
+			STDERR_FILENO);
+	else if (code == 2)
+		ft_putstr_fd(
+			"\033[0;31msyntax error\033[0m: invalid token after heredoc",
+			STDERR_FILENO);
+	else if (code == 3)
+		ft_putstr_fd("\033[0;31msyntax error\033[0m: invalid token after pipe",
+			STDERR_FILENO);
+	else if (code == 4)
+		ft_putstr_fd("\033[0;31msyntax error\033[0m: invalid argument syntax",
+			STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	minishell->last_exit_status = 1;
+	return (1);
+}
+
+/**
+ * Printing execution errors
+ */
+char	*p_error(char *s1, char *s2, char *s3)
+{
+	char	*temp;
+
+	if (s3 == NULL)
+	{
+		temp = ft_strjoin(s1, s2);
+		perror(temp);
+	}
+	else
+	{
+		temp = ft_strjoin_three(s1, s2, s3);
+		ft_putendl_fd(temp, STDERR_FILENO);
+	}
+	free(temp);
+	return (NULL);
+}
