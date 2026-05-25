6be46daea29f35937b211269792a6877208c5b53
marcogmurciano <marcogmurciano@gmail.com>
2026-05-25 13:23:15 +0200

    fix: Implement memory management and cleanup

---
+++ src/freeing_utils/environ_list.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environ_list.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 17:49:46 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:34:40 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Frees the minishell structure
+ */
+void	free_environment(t_env **env_head)
+{
+	t_env	*current_env;
+	t_env	*temp_env;
+
+	if (env_head == NULL || *env_head == NULL)
+	{
+		return ;
+	}
+	current_env = *env_head;
+	while (current_env)
+	{
+		if (current_env->key)
+			free(current_env->key);
+		if (current_env->value)
+			free(current_env->value);
+		current_env->key = NULL;
+		current_env->value = NULL;
+		temp_env = current_env;
+		current_env = current_env->next;
+		free(temp_env);
+		temp_env = NULL;
+	}
+	*env_head = NULL;
+}
---
+++ src/freeing_utils/minishell.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 17:50:40 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:33:49 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Closes the duplicated STDIN and STDOUT file descriptors
+ */
+void	close_dup_stds(t_minishell *minishell)
+{
+	if (minishell->duplicated_std_fds[0] != -1)
+	{
+		close(minishell->duplicated_std_fds[0]);
+		minishell->duplicated_std_fds[0] = -1;
+	}
+	if (minishell->duplicated_std_fds[1] != -1)
+	{
+		close(minishell->duplicated_std_fds[1]);
+		minishell->duplicated_std_fds[1] = -1;
+	}
+}
+
+/**
+ * Frees the environment list
+ */
+static void	free_env(t_minishell *minishell)
+{
+	if (minishell->envp)
+	{
+		ft_free_array((void **)minishell->envp);
+		minishell->envp = NULL;
+	}
+	if (minishell->environment)
+	{
+		free_environment(&(minishell->environment));
+		minishell->environment = NULL;
+	}
+}
+
+/**
+ * Frees the minishell structure
+ */
+void	free_minishell(t_minishell *minishell)
+{
+	if (minishell->input)
+	{
+		free(minishell->input);
+		minishell->input = NULL;
+	}
+	if (minishell->tokens_list)
+	{
+		free_tokens_list(&(minishell->tokens_list));
+		minishell->tokens_list = NULL;
+	}
+	if (minishell->cmd_pipelines)
+	{
+		free_cmds(&(minishell->cmd_pipelines));
+		minishell->cmd_pipelines = NULL;
+	}
+	free(minishell->lastdir);
+	free_env(minishell);
+	close_dup_stds(minishell);
+}
