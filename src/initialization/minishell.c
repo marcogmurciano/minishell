cc5d559f8dd361d8364ef7c4493f468944f5638a
marcogmurciano <marcogmurciano@gmail.com>
2026-05-25 13:23:02 +0200

    feat: Implement initialization and main loop

---
+++ src/initialization/minishell.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 10:01:27 by dbarba-v          #+#    #+#             */
+/*   Updated: 2025/07/15 11:32:10 by dbarba-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ * Initialize the minishell structure
+ */
+void	init_mini(t_minishell *minishell, int argc, char **argv, char **envp)
+{
+	char	*cwd;
+
+	ft_bzero(minishell, sizeof(t_minishell));
+	minishell->environment = regenerate_environment(envp);
+	minishell->environment = check_environment(minishell);
+	minishell->envp = get_environment_array(minishell->environment);
+	minishell->argv = argv;
+	minishell->argc = argc;
+	minishell->pid = -1;
+	minishell->last_exit_status = 0;
+	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+		minishell->lastdir = ft_strdup("/");
+	else
+		minishell->lastdir = cwd;
+	minishell->duplicated_std_fds[0] = dup(STDIN_FILENO);
+	minishell->duplicated_std_fds[1] = dup(STDOUT_FILENO);
+}
